dd5ca06cc1560c80deb2592c747115a9bd3c3a1d
Иван <qjester@live.ru>
2016-02-16 16:08:34 +0600

    AHAH

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Huyou, World!");
+            Console.WriteLine("HAIL HITLEEER");
             Console.ReadKey();
         }
     }
